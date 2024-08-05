7b2b40f78df7a0dcf4bb630dab7d74e1dfbc5595
Scorhenao <samcorhenao@gmail.com>
2024-08-05 19:21:04 -0400

    new routes

--- src/routes/Router.ts
+++ src/routes/Router.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express'; // Importa el objeto Router de Express para definir rutas.
-import { userRouter,  authRouter, orderRouter } from './'; // Importa los routers de usuarios y tareas desde el archivo 'index.ts' en la misma carpeta.
+import { userRouter,  authRouter, orderRouter, productRouter } from './'; // Importa los routers de usuarios y tareas desde el archivo 'index.ts' en la misma carpeta.
 
 const router = Router(); // Crea una nueva instancia de Router.
 
 router.use('/users', userRouter); // Define que todas las rutas que comienzan con '/users' serán manejadas por 'userRouter'.
 router.use('/auth', authRouter);
 router.use('/orders', orderRouter);
+router.use('/products',productRouter)
 
 
 export default router; // Exporta el router configurado para su uso en otros archivos.
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,3 +1,4 @@
 export * from './userRoutes'; // Exporta todos los elementos de 'userRoutes.ts', permitiendo su uso en otros archivos que importen desde este archivo.
 export * from './authRoutes';
 export * from './orderRoutes';
+export * from './productRoutes';
